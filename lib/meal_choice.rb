f91fe884cbc72e99e636777e9059ca37b4518a8a
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-11 20:30:58 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,7 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+def meal(veg1,veg2,protien)
+  meal_string = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal_string
+  return meal_string
+end
